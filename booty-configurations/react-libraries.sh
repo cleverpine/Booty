da65f891dfd5c9c19cab054f8f899930c182cce5
Borislav Kibarov <borislav.kibarov@cleverpine.com>
2024-01-31 14:23:13 +0200

    Add react library descriptions for 'emotion' packages

--- booty-configurations/react-libraries.sh
+++ booty-configurations/react-libraries.sh
@@ -16,4 +16,11 @@ readonly react_libs_descriptions=(
     "Material UI is an open-source React component library that implements Google's Material Design.
      It's comprehensive and can be used in production out of the box.\n
      Docs: \033[4mhttps://github.com/mui/material-ui\033[0m"
+     #2
+     "Simple styling in React.
+     Docs: \033[4mhttps://github.com/emotion-js\033[0m"
+     #3
+     "The styled API for @emotion/react.
+     Docs: \033[4mhttps://github.com/emotion-js\033[0m"
+
 )
